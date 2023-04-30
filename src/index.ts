a0896535cb5623bb55e9955b391d7920af604d3e
Sernin van de Krol <serninpc@paneidos.net>
2023-04-30 20:28:29 +0200

    Initial plugin with ref function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export default {};
+import Serverless from "serverless";
+import Plugin from "serverless/classes/Plugin";
+
+class MoreVariablesPlugin implements Plugin {
+    commands: Plugin.Commands | undefined;
+    hooks: Plugin.Hooks;
+    variableResolvers: Plugin.VariableResolvers | undefined;
+    serverless: Serverless
+    configurationVariablesSources: any
+
+    constructor(serverless: Serverless, options: Serverless.Options, { log }: Plugin.Logging) {
+        this.serverless = serverless;
+        this.hooks = {};
+        this.configurationVariablesSources = {
+            ref: {
+                resolve({ params }: { params: unknown[] }) {
+                    if (typeof params[0] !== 'string') {
+                        log.error('Expected resource name as argument to ref()');
+                    } else {
+                        return {value: {Ref: params[0]}};
+                    }
+                }
+            }
+        }
+    }
+}
+
+export = MoreVariablesPlugin;
