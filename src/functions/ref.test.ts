18b005a0083d202156fb75dd669a917d76dbe445
Sernin van de Krol <serninpc@paneidos.net>
2023-05-01 06:33:45 +0000

    Add test for calling ref() without string

--- src/functions/ref.test.ts
+++ src/functions/ref.test.ts
@@ -1,9 +1,16 @@
 import ref from './ref';
 
-const callRef = function(...params: string[]) {
+const callRef = function(...params: unknown[]) {
     return ref.resolve({ params }).value;
 };
 
-test('ref() returns object', () => {
-    expect(callRef('HttpApi')).toEqual({Ref: 'HttpApi'});
+describe('ref()', () => {
+    it('returns an object', () => {
+        expect(callRef('HttpApi')).toEqual({Ref: 'HttpApi'});
+    });
+
+    it('fails on non-string input', () => {
+        expect(() => callRef(null)).toThrow();
+    });
 });
+
