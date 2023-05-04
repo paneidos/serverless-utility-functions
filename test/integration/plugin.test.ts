9537f4bf98e9c6e838b13c879f0fbcac80377799
Sernin van de Krol <serninpc@paneidos.net>
2023-05-04 16:57:52 +0000

    Add test for plugin

--- test/integration/plugin.test.ts
+++ test/integration/plugin.test.ts
@@ -1,7 +1,10 @@
 import Serverless from "serverless";
+const resolve = require("serverless/lib/configuration/variables/resolve");
+const resolveMeta = require("serverless/lib/configuration/variables/resolve-meta");
 import UtilityFunctionsPlugin from '../../src/index';
+import ref from '../../src/functions/ref';
 
-function buildSls() {
+function buildSls(): Serverless {
     const sls = new Serverless({
         commands: [],
         options: {}
@@ -13,5 +16,24 @@ function buildSls() {
 describe('UtilityFunctionsPlugin', () => {
     it('installs', () => {
         expect(buildSls()).not.toBeNull();
+    })
+
+    it('provides ref()', async () => {
+        let variablesMeta;
+        let configuration = {
+            quotedString: "${ref('HttpApi')}",
+            unquotedString: "${ref(HttpApi)}",
+        };
+        variablesMeta = resolveMeta(configuration);
+        await resolve({
+            serviceDir: process.cwd(),
+            configuration,
+            variablesMeta,
+            sources: { ref },
+            options: {},
+            fulfilledSources: new Set(['ref']),
+        });
+        expect(configuration.quotedString).toEqual({ Ref: 'HttpApi'});
+        expect(configuration.unquotedString).toEqual({ Ref: 'HttpApi'});
     })
 })
